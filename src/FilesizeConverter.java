c821ccab4f80dc19a0532620ba3701c33d4763a4
Simon Vetter <vetter@esa.tu-darmstadt.de>
2023-10-09 14:25:55 +0200

    Added GPL3 license

--- src/FilesizeConverter.java
+++ src/FilesizeConverter.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2011, 2021, 2023 Simon Vetter
+ *
+ * This file is part of VideoCalcSuite.
+ *
+ * VideoCalcSuite is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * VideoCalcSuite is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with VideoCalcSuite.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package vcs;
 
 import javax.swing.*;
